4a598cc399effc08868199e9beb7668670aad68a
Chamara Senarath <senarath.chamara@gmail.com>
2019-11-19 14:47:45 +0530

    few css tweaks in splash editor

--- src/components/SplashEditor.vue
+++ src/components/SplashEditor.vue
@@ -1,57 +1,64 @@
 <template>
-  <v-layout row>
-    <v-flex>
-      <div ref="canvas">
-        <v-card
-          class="mb-12"
-          outlined
-          elevation="4"
-          width="1280px"
-          height="720px"
-        >
-          <v-container>
-            <v-layout>
-              <Moveable
-                v-for="title in titleList"
-                :key="title.id"
-                class="moveable"
-                v-bind="moveable"
-                @drag="handleDrag"
-              >
-                <span>{{ title.text }}</span>
-              </Moveable>
-            </v-layout>
-          </v-container>
-        </v-card>
-      </div>
-    </v-flex>
-    <v-flex>
-      <v-layout v-for="title in titleList" :key="title.id" row align-center>
-        <v-flex>
-          <v-text-field
-            color="green darken-3"
-            prepend-icon="fa fa-edit"
-            label="Text"
-            v-model="title.text"
-          ></v-text-field>
-        </v-flex>
-        <v-flex>
-          <v-btn
-            @click="deleteTitle(title.id)"
-            dark
-            color="red darken-3"
-            class="mx-2"
-            fab
-            small
-            ><v-icon small dark>fa fa-trash-alt</v-icon></v-btn
+  <v-container fluid>
+    <v-layout row>
+      <v-flex>
+        <div ref="canvas">
+          <v-card
+            class="mb-12"
+            outlined
+            elevation="4"
+            width="1280px"
+            height="720px"
           >
-        </v-flex>
-      </v-layout>
-      <v-btn dark color="green darken-3" block @click="addNewTitle"
-        >Add New Title <v-icon right>fa fa-plus</v-icon></v-btn
-      >
-    </v-flex>
-  </v-layout>
+            <v-container>
+              <v-layout>
+                <Moveable
+                  v-for="title in titleList"
+                  :key="title.id"
+                  class="moveable"
+                  v-bind="moveable"
+                  @drag="handleDrag"
+                >
+                  <span>{{ title.text }}</span>
+                </Moveable>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </div>
+      </v-flex>
+
+      <v-flex>
+        <v-layout column align-center>
+          <v-layout v-for="title in titleList" :key="title.id" row align-center>
+            <v-flex>
+              <v-text-field
+                color="green darken-3"
+                prepend-icon="fa fa-edit"
+                label="Text"
+                v-model="title.text"
+              ></v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-btn
+                @click="deleteTitle(title.id)"
+                dark
+                color="red darken-3"
+                class="mx-2"
+                fab
+                small
+                ><v-icon small dark>fa fa-trash-alt</v-icon></v-btn
+              >
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-btn dark color="green darken-3" block @click="addNewTitle"
+              >Add New Title <v-icon right>fa fa-plus</v-icon></v-btn
+            >
+          </v-layout>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 import Moveable from "vue-moveable";
@@ -101,10 +108,7 @@ export default {
     }
   },
   mounted() {
-    this.$frame = new Frame({
-      left: "0px",
-      top: "0px"
-    });
+    this.$frame = new Frame();
   }
 };
 </script>
@@ -114,16 +118,12 @@ export default {
   position: relative;
   text-align: center;
   font-size: 40px;
-  margin: 0 auto;
   font-weight: 100;
   letter-spacing: 1px;
 }
 
 .moveable span {
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   white-space: nowrap;
 }
 </style>
