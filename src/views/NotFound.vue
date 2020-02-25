f841db23510c615301722e11eccc72b9ddef546a
Chamara Senarath <senarath.chamara@gmail.com>
2020-02-25 12:42:33 +0530

    added go home button to 404 not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -5,5 +5,6 @@
     <p class="headline">
       Sorry, We can not find what you are searching for
     </p>
+    <v-btn to="/" rounded dark depressed color="#00868E">Go Home</v-btn>
   </v-layout>
 </template>
