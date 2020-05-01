fa643ffdd1aa9e29d9ac342eb2647c79c336c273
Chamara Senarath <senarath.chamara@gmail.com>
2020-05-01 19:18:56 +0530

    fixed insights

--- src/views/Insight.vue
+++ src/views/Insight.vue
@@ -1,9 +1,5 @@
 <template>
   <v-container fluid>
-    <EditRange
-      @saveRange="saveRange"
-      :editRangeDialog="editRangeDialog"
-    ></EditRange>
     <v-layout row>
       <v-flex v-for="(card, index) in cardList" :key="index" xs12 md3 pa-2>
         <v-card dark :color="`${card.color} lighten-1`">
@@ -23,10 +19,11 @@
         </v-card>
       </v-flex>
     </v-layout>
-    <v-layout mt-2 row>
+    <v-layout mt-2 row justify-center>
       <v-flex
         xs12
-        md4
+        md6
+        xl4
         pa-2
         v-for="(pieChart, index) in pieChartList"
         :key="index"
@@ -36,17 +33,6 @@
             <v-layout row justify-space-between>
               <v-flex xs8>
                 {{ $t("Views By") }} {{ $t(pieChart.title) }}
-              </v-flex>
-              <v-spacer></v-spacer>
-              <v-flex xs3>
-                <v-btn
-                  dark
-                  @click="editRangeDialog = true"
-                  small
-                  v-if="pieChart.title == 'Age' && showPieCharts"
-                  color="blue"
-                  >{{ $t("Edit Range") }}</v-btn
-                >
               </v-flex>
             </v-layout>
           </v-card-title>
@@ -65,12 +51,10 @@
 
 <script>
 import VueApexCharts from "vue-apexcharts";
-import EditRange from "@/components/EditRange";
 import axios from "axios";
 export default {
   components: {
     VueApexCharts,
-    EditRange
   },
   data() {
     return {
@@ -83,98 +67,87 @@ export default {
           title: "Total Views",
           color: "green",
           icon: "clock",
-          value: null
+          value: null,
         },
         {
           title: "Views in Last Week",
           color: "purple",
           icon: "clock",
-          value: null
+          value: null,
         },
         {
           title: "Views in Last Month",
           color: "red",
           icon: "clock",
-          value: null
+          value: null,
         },
         {
           title: "Views in Last Year",
           color: "blue",
           icon: "clock",
-          value: null
-        }
+          value: null,
+        },
       ],
       pieChartList: [
         {
+          title: "Team",
+          options: {
+            labels: [],
+          },
+          series: [],
+        },
+        {
+          title: "Position",
+          options: {
+            labels: [],
+          },
+          series: [],
+        },
+        {
           title: "Gender",
           options: {
-            labels: []
+            labels: [],
           },
-          series: []
+          series: [],
         },
-        {
-          title: "Age",
-          options: {
-            labels: []
-          },
-          series: []
-        },
-        {
-          title: "Location",
-          options: {
-            labels: []
-          },
-          series: []
-        }
-      ]
+      ],
     };
   },
   methods: {
-    saveRange(val) {
-      this.editRangeDialog = false;
-      this.rangeList = val.toString();
-      for (let i = 0; i < 3; i++) {
-        this.pieChartList[i].options.labels = [];
-        this.pieChartList[i].series = [];
-      }
-      this.fetchData();
-    },
     async fetchData() {
       this.showPieCharts = false;
       try {
         let result = await axios.get(
-          this.API_URL +
-            `/api/insight/views?vid=${this.vid}&range=${this.rangeList}`
+          this.API_URL + `/api/insight/views?vid=${this.vid}`
         );
         this.cardList[0].value = result.data.totalViews;
         this.cardList[1].value = result.data.lastWeekViews;
         this.cardList[2].value = result.data.lastMonthViews;
         this.cardList[3].value = result.data.lastYearViews;
-        result.data.viewsByGender.forEach(gender => {
-          this.pieChartList[0].options.labels.push(gender.label);
-          this.pieChartList[0].series.push(gender.value);
+
+        result.data.viewsByTeam.forEach((team) => {
+          this.pieChartList[0].options.labels.push(team.label);
+          this.pieChartList[0].series.push(team.value);
         });
-        result.data.viewsByAge.forEach(age => {
-          if (age.value != 0) {
-            this.pieChartList[1].options.labels.push(age.label);
-            this.pieChartList[1].series.push(age.value);
-          }
+        result.data.viewsByPosition.forEach((position) => {
+          this.pieChartList[1].options.labels.push(position.label);
+          this.pieChartList[1].series.push(position.value);
         });
-        result.data.viewsByLocation.forEach(location => {
-          this.pieChartList[2].options.labels.push(location.label);
-          this.pieChartList[2].series.push(location.value);
+        result.data.viewsByGender.forEach((gender) => {
+          this.pieChartList[2].options.labels.push(gender.label);
+          this.pieChartList[2].series.push(gender.value);
         });
         this.showPieCharts = true;
       } catch (error) {
         console.log(error);
       }
-    }
+    },
   },
   mounted() {
     this.vid = this.$route.params.vid;
     if (this.vid != null) {
       this.fetchData();
     }
-  }
+  },
 };
 </script>
